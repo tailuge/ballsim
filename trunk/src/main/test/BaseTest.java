cf6e387689ded4fea40e10a543a7725db9d8db5b
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-06-30 08:07:02 +0000

    project file in, but wrong place.

---
+++ trunk/src/main/test/BaseTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class BaseTest 
+{
+
+	
+	 @Test
+     public final void testObject() 
+     {
+             
+             Assert.assertNotNull("object ok", 1);           
+     }
+}
