39929f2996cd967e3943e336293073c003c4ad78
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-01 22:49:34 +0000

    maven holder

---
+++ maven/src/main/java/maventest/test/App.java
@@ -0,0 +1,20 @@
+package maventest.test;
+
+import  org.apache.commons.math.geometry.Vector3D;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Vector3D a = new Vector3D(1,2,3);
+
+        System.out.println( a );
+
+    }
+}
