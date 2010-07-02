9ff2319a1fce272e1575c459c46d23fb6b57a13a
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-02 12:50:41 +0000

    event progression methods

--- trunk/org/src/main/java/ballsim/org/Ball.java
+++ trunk/org/src/main/java/ballsim/org/Ball.java
@@ -15,48 +15,10 @@ public class Ball
 		
 	private List<Event> events = new ArrayList<Event>();
 	
-	void advance(double t)
-	{
-		return;
-	}
-
-	public StateTime timeUntilNextState()
-	{
-		return new StateTime();
-	}
-	
-	
 	public void initialise(Vector3D pos_, Vector3D vel_)
 	{
-		Event e = new Event(pos_,vel_,new Vector3D(0,0,0),new Vector3D(0,0,0), new StateTime(), EventType.InitialHit);
+		Event e = new Event(pos_,vel_,new Vector3D(0,0,0),new Vector3D(0,0,0), new Vector3D(0,0,0),new StateTime(), EventType.InitialHit);
 		events.add(e);
-	}
-
-	public int computeNaturalEvents()
-	{
-		// if state is rolling, compute stationary time
-		
-		return 0;
-	}
-
-	public Vector3D getPos(double t) 
-	{
-		return events.get(0).pos;
-	}
-
-	public Vector3D getVel(double t) 
-	{
-		return events.get(0).vel;
-	}
-
-	public Vector3D getSpin(double t) 
-	{
-		return events.get(0).spin;
-	}
-
-	public State getState(double t) 
-	{
-		return events.get(0).statetime.state;
 	}
 
 	public List<Event> getEvents() 
@@ -64,60 +26,5 @@ public class Ball
 		return events;
 	}
 
-	
-	// initial state - if not in equilibrium, set as sliding
-	// given sliding state - progress to rolling
-	// given rolling progress to stationary
-	// given either progress to cushion
-	// given any progress to collision
-	
-	
-	public static double timeToStopRolling(final Event e)
-	{
-
-		// v = v0 + a * t   when v = 0
-		// acceleration is rolling friction
-		// t = -v0/a
-		
-		double magnitude = e.vel.getNorm();
-		return -magnitude / accelRoll;		
-	}
-	
-
-	public static Event stationaryEvent(final Event e)
-	{
-	
-		assert(e.statetime.state == State.Rolling);
-		double t = timeToStopRolling(e);
-		Event result = new Event(e);
-		result = advanceRollingDelta(result,t);
-		result.statetime.state = State.Stationary;
-		return result;
-	}
-
-	// when rolling acceleration opposes roll
-	public static Vector3D getRollingAccelerationVector(Event e)
-	{
-		Vector3D result = new Vector3D(1,e.vel);
-		result.normalize();
-		result.scalarMultiply(accelRoll);
-		return result;
-	}
-	
-	public static Event advanceRollingDelta(Event e, double t)
-	{
-		// v = v0 + a * t   when v = 0
-		// p = p0 + ut + 1/2att
-		
-		//update pos
-		
-		// update vel
-		
-		Vector3D result = new Vector3D(1,e.vel);
-		Vector3D acc = getRollingAccelerationVector(e);
-		acc.scalarMultiply(t);
-		result.add(acc);
-		return e;
-	}
 	
 }
