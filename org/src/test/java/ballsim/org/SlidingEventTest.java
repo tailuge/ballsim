27979d7c3e5ba0dd43671ab7648e91fdf4c8243e
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-03 12:20:05 +0000

    upgrade to junit4

---
+++ org/src/test/java/ballsim/org/SlidingEventTest.java
@@ -0,0 +1,54 @@
+package ballsim.org;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SlidingEventTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public final void testGetSimpleEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAccelerationVector() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAngularAccelerationVector() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdvanceRollingDelta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testTimeToStopRolling() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testStationaryEventFromRolling() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInfereState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
