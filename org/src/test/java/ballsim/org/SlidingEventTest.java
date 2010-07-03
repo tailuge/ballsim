62bf5a1d67fbf050e730ae5bd286f1890461bea1
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-03 12:29:26 +0000

    start sliding tests

--- org/src/test/java/ballsim/org/SlidingEventTest.java
+++ org/src/test/java/ballsim/org/SlidingEventTest.java
@@ -2,23 +2,28 @@ package ballsim.org;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.math.geometry.Vector3D;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SlidingEventTest {
 
+	private Event masse;
+	private Event slide;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp()
+	{
+		slide = Utilities.getSliding(Vector3D.PLUS_I, Vector3D.ZERO);
+		masse = Utilities.getSliding(Vector3D.ZERO, Vector3D.PLUS_I);
 	}
 
-	@Test
-	public final void testGetSimpleEvent() {
-		fail("Not yet implemented"); // TODO
-	}
 
 	@Test
 	public final void testGetAccelerationVector() {
-		fail("Not yet implemented"); // TODO
+		assertTrue("Sliding ball has acceleration",slide.getAccelerationVector().getNorm() > 0);
+		assertTrue("Sliding masse ball has acceleration",masse.getAccelerationVector().getNorm() > 0);
+		assertEquals("Sliding,masse have same magnitude of acceleration",slide.getAccelerationVector().getNorm() , masse.getAccelerationVector().getNorm(), 0.0);
 	}
 
 	@Test
