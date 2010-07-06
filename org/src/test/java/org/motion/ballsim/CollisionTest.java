9553bdfd1b87c57a3c5e093e85fdde98f49e4b9a
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-06 14:49:15 +0000

    start collision tests

---
+++ org/src/test/java/org/motion/ballsim/CollisionTest.java
@@ -0,0 +1,20 @@
+package org.motion.ballsim;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.math.geometry.Vector3D;
+import org.junit.Test;
+
+public class CollisionTest {
+
+	@Test
+	public final void testCollisionTime() 
+	{
+		Event e1 = Utilities.getRolling(Vector3D.PLUS_I.scalarMultiply(30));
+		Event e2 = Utilities.getRolling(Vector3D.PLUS_I.scalarMultiply(-30));
+		e2.pos = Vector3D.PLUS_I.scalarMultiply(Ball.R*3);
+		double t = Collision.collisionTime(e1, e2);
+		assertTrue("Rolling towards each other collide",t>0);
+	}
+
+}
