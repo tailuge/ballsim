0ea9f44b09a121187e5d1482bcdd0f89dc7d221c
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-17 12:58:59 +0000

    more work on shot search

--- org/src/test/java/org/motion/ballsim/StaticPlotApp.java
+++ org/src/test/java/org/motion/ballsim/StaticPlotApp.java
@@ -15,19 +15,28 @@ public class StaticPlotApp
 //		Ball b2 = new Ball(Utilities.getStationary(new Vector3D(-4*Ball.R,4.5*Ball.R,0)));
 		Ball b2 = new Ball(Utilities.getRolling(Vector3D.MINUS_J.scalarMultiply(300) , new Vector3D(Ball.R*7.6,Ball.R*4,0)));
 		Ball b1 = new Ball(Utilities.getStationary(new Vector3D(Ball.R*6,-Ball.R*6,0)));
-		Ball b3 = new Ball(Utilities.getStationary(new Vector3D(-Ball.R*1,Ball.R*25,0)));
+		Ball b3 = new Ball(Utilities.getStationary(new Vector3D(Ball.R*5,-Ball.R*3,0)));
 		
 		//Ball b1 = new Ball(Utilities.getRolling(Vector3D.MINUS_I.scalarMultiply(30) ));
 		//Ball b2 = new Ball(Utilities.getRolling(Vector3D.MINUS_I.scalarMultiply(30) , Vector3D.PLUS_J.scalarMultiply(Ball.R*4)));
 
+		Vector3D dir = new Vector3D(Math.PI/2.0+2*Math.PI*(double)47/(double)360,0);
+		b2.lastEvent().vel = dir.scalarMultiply(280);
+		b2.lastEvent().angularVel = dir.scalarMultiply(1);
+		b2.lastEvent().state = State.Sliding;
 		
 		t.balls.add(b1);
 		t.balls.add(b2);
 		t.balls.add(b3);
-		t.generateSequence();
+		
+		ShotFinder finder = new ShotFinder(new OneCushionRuleSet(),t);
+		
+		Table tResult = finder.FindBest(b2, 360);
+		
+		//t.generateSequence();
 		//t.generateNext();
 
-		plot = new StaticPlot(t,50);
+		plot = new StaticPlot(tResult,50);
     	plot.draw();
     }	
 
