7c4b2b8eea49416522fe7f011d32799f93911feb
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-23 04:36:41 +0000

    fix for sliding ball -> rolling in zero time.

--- org/src/test/java/org/motion/ballsim/plotter/MultiPlotApp.java
+++ org/src/test/java/org/motion/ballsim/plotter/MultiPlotApp.java
@@ -5,35 +5,32 @@ import java.util.Collection;
 
 import org.apache.commons.math.geometry.Vector3D;
 import org.motion.ballsim.Ball;
-import org.motion.ballsim.OneCushionRuleSet;
-import org.motion.ballsim.ShotFinder;
 import org.motion.ballsim.Table;
+import org.motion.ballsim.UtilEvent;
 import org.motion.ballsim.Utilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MultiPlotApp {
 
 	static StaticPlot plot;
 	
+	private final static Logger logger = LoggerFactory.getLogger(MultiPlotApp.class);
+
     public static void main(String[] args) 
     {
-    	
-		Table t = new Table();
-		t.ball(1).setFirstEvent(Utilities.getStationary(new Vector3D(Ball.R*6,-Ball.R*16,0)));
-		t.ball(2).setFirstEvent(Utilities.getStationary(new Vector3D(-Ball.R*6,-Ball.R*6,0)));
-		t.ball(3).setFirstEvent(Utilities.getStationary(new Vector3D(Ball.R*5,-Ball.R*3,0)));
-		ShotFinder finder = new ShotFinder(new OneCushionRuleSet(),t);		
-		Table tResult = finder.FindBest(t.ball(1),25);
-
-		Table t2 = new Table();
-		t2.ball(1).setFirstEvent(Utilities.getStationary(new Vector3D(Ball.R*6,-Ball.R*10,0)));
-		t2.ball(2).setFirstEvent(Utilities.getStationary(new Vector3D(-Ball.R*6,-Ball.R*6,0)));
-		t2.ball(3).setFirstEvent(Utilities.getStationary(new Vector3D(Ball.R*5,-Ball.R*3,0)));
-		ShotFinder finder2 = new ShotFinder(new OneCushionRuleSet(),t2);
-		Table tResult2 = finder2.FindBest(t2.ball(1),26);
-
 		Collection<Table> tables = new ArrayList<Table>();
-		tables.add(tResult);
-		tables.add(tResult2);
+    	double h = -1.5;
+    	while(h < 1.5)
+    	{
+    		logger.info("h:{}",h);
+			Table t = new Table();
+			t.ball(1).setFirstEvent(UtilEvent.hit(Vector3D.ZERO, Vector3D.MINUS_J, 100,h));			
+			t.ball(2).setFirstEvent(Utilities.getStationary(new Vector3D(Ball.R,-Ball.R * 6, 0)));
+			t.generateSequence();
+			tables.add(t);		
+			h += 0.3;
+    	}
 		
 		plot = new StaticPlot(tables,50);
     	plot.draw();
