f46d3be1aa5013eb4a744a4ee51770fd524abc66
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-24 11:31:10 +0000

    tidy up jscript

--- org/web/lib/billiardtable.js
+++ org/web/lib/billiardtable.js
@@ -1,3 +1,82 @@
+function Ball()
+{
+    var x;
+    var y;
+    var angx;
+    var angy;
+    var colour;
+    var id;
+    var radius = 10;
+ 	var canvas;
+ 	var ctx;
+    
+    this.draw = draw;
+	this.init = init;
+    
+	function init(id_,colour_,x_,y_)
+	{
+        canvas = document.getElementById('billiardtable');	
+        ctx = canvas.getContext('2d');
+        x = x_;
+        y = y_;
+        id = id_;
+        colour = colour_;
+	}
+
+	function draw()
+	{
+		drawShadow();
+		drawBall();
+		drawShine();
+	}
+	    	
+	function drawBall()
+	{
+    	ctx.shadowOffsetX = 0;
+		ctx.shadowOffsetY = 0;
+		ctx.shadowBlur = 0;
+    	ctx.beginPath();
+    	ctx.moveTo(x+radius, y);
+	    ctx.arc(x, y, radius, 0, Math.PI*2, true);
+    	ctx.fill();
+    	ctx.stroke();
+    	
+    }	
+    
+   	function drawShadow()
+	{
+    	ctx.fillStyle = colour;
+    	ctx.strokeStyle = colour;
+        ctx.shadowOffsetX = 1;
+		ctx.shadowOffsetY = 1;
+		ctx.shadowBlur = 4;
+		ctx.shadowColor = 'black';
+    	ctx.beginPath();
+    	ctx.moveTo(x+radius, y);
+	    ctx.arc(x, y, radius, 0, Math.PI*2, true);
+    	ctx.fill();
+    	ctx.stroke();
+	} 
+
+   	function drawShine()
+	{
+    	ctx.fillStyle = 'white';
+    	ctx.strokeStyle = 'white';
+        ctx.shadowOffsetX = 1;
+		ctx.shadowOffsetY = 1;
+		ctx.shadowBlur = 5;
+		ctx.shadowColor = 'white';
+		
+		var o = 3;
+    	ctx.beginPath();
+    	ctx.moveTo(x-o, y-o);
+	    ctx.arc(x-o, y-o, 1, 0, Math.PI*2, true);
+    	ctx.fill();
+    	ctx.stroke();
+	} 
+}
+	    
+
 function BilliardTable()
 { 
  	var canvas;
@@ -7,23 +86,32 @@ function BilliardTable()
 	 	
 	this.draw = draw;
 	this.init = init;
+
+    var ball1 = new Ball();
+    var ball2 = new Ball();
+    var ball3 = new Ball();
 		
 	function init()
 	{
         canvas = document.getElementById('billiardtable');	
         ctx = canvas.getContext('2d');
-        ctx.shadowOffsetX = 2;
-		ctx.shadowOffsetY = 2;
-		ctx.shadowBlur = 10;
+        ctx.shadowOffsetX = 1;
+		ctx.shadowOffsetY = 1;
+		ctx.shadowBlur = 2;
 		ctx.shadowColor = "black";
    		center = canvas.width/2;
     	scale = center * 0.8;
+    	
+		ball1.init(1,'white',190,290);
+		ball2.init(2,'red',90,90);
+		ball3.init(3,'yellow',150,370);
+    	
 	    canvas.onmousedown = function(event)
     	{
-    		spinY = (event.offsetY - canvas.height/2) / (canvas.height/2);
     		draw();
 		}
 		draw();
+		
 	}
 
 	function draw()
@@ -33,10 +121,14 @@ function BilliardTable()
     
     	ctx.beginPath();
     	ctx.moveTo(0,0);
-    	ctx.fillStyle = 'lightblue';
+    	ctx.fillStyle = 'blue';
     	ctx.fillRect(0,0,canvas.width,canvas.height)
     	ctx.fill();
     	ctx.stroke();    
+    	
+    	ball1.draw();
+    	ball2.draw();
+    	ball3.draw();
 	}	
 }
 
