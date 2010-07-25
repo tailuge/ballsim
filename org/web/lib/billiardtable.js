dc8ca9d79b646c15d6bc813ac449e0fbc3a2b3cf
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-07-25 01:19:56 +0000

    aiming on canvas done.

--- org/web/lib/billiardtable.js
+++ org/web/lib/billiardtable.js
@@ -1,81 +1,4 @@
-function Ball()
-{
-    var x;
-    var y;
-    var angx;
-    var angy;
-    var colour;
-    var id;
-    var radius = 10;
- 	var canvas;
- 	var ctx;
-    
-    this.draw = draw;
-	this.init = init;
-    
-	function init(id_,colour_,x_,y_)
-	{
-        canvas = document.getElementById('billiardtable');	
-        ctx = canvas.getContext('2d');
-        x = x_;
-        y = y_;
-        id = id_;
-        colour = colour_;
-	}
 
-	function draw()
-	{
-		drawShadow();
-		drawBall();
-		drawShine();
-	}
-	    	
-	function drawBall()
-	{
-    	ctx.shadowOffsetX = 0;
-		ctx.shadowOffsetY = 0;
-		ctx.shadowBlur = 0;
-    	ctx.beginPath();
-    	ctx.moveTo(x+radius, y);
-	    ctx.arc(x, y, radius, 0, Math.PI*2, true);
-    	ctx.fill();
-    	ctx.stroke();
-    	
-    }	
-    
-   	function drawShadow()
-	{
-    	ctx.fillStyle = colour;
-    	ctx.strokeStyle = colour;
-        ctx.shadowOffsetX = 1;
-		ctx.shadowOffsetY = 1;
-		ctx.shadowBlur = 4;
-		ctx.shadowColor = 'black';
-    	ctx.beginPath();
-    	ctx.moveTo(x+radius, y);
-	    ctx.arc(x, y, radius, 0, Math.PI*2, true);
-    	ctx.fill();
-    	ctx.stroke();
-	} 
-
-   	function drawShine()
-	{
-    	ctx.fillStyle = 'white';
-    	ctx.strokeStyle = 'white';
-        ctx.shadowOffsetX = 1;
-		ctx.shadowOffsetY = 1;
-		ctx.shadowBlur = 5;
-		ctx.shadowColor = 'white';
-		
-		var o = 3;
-    	ctx.beginPath();
-    	ctx.moveTo(x-o, y-o);
-	    ctx.arc(x-o, y-o, 1, 0, Math.PI*2, true);
-    	ctx.fill();
-    	ctx.stroke();
-	} 
-}
-	    
 
 function BilliardTable()
 { 
@@ -90,6 +13,8 @@ function BilliardTable()
     var ball1 = new Ball();
     var ball2 = new Ball();
     var ball3 = new Ball();
+
+    var cue = new Cue();
 		
 	function init()
 	{
@@ -105,11 +30,9 @@ function BilliardTable()
 		ball1.init(1,'white',190,290);
 		ball2.init(2,'red',90,90);
 		ball3.init(3,'yellow',150,370);
+
+		cue.init(190,290);
     	
-	    canvas.onmousedown = function(event)
-    	{
-    		draw();
-		}
 		draw();
 		
 	}
@@ -129,6 +52,8 @@ function BilliardTable()
     	ball1.draw();
     	ball2.draw();
     	ball3.draw();
+    	
+    	cue.draw();
 	}	
 }
 
