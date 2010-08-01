e1afff156d9adecb3cdc4e0d25bf650e1dd0e3a7
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-08-01 03:50:05 +0000

    tidy up

--- trunk/ballsimapp/src/org/motion/ballsimapp/client/Ballsimapp.java
+++ trunk/ballsimapp/src/org/motion/ballsimapp/client/Ballsimapp.java
@@ -2,83 +2,34 @@ package org.motion.ballsimapp.client;
 
 
 
-import java.util.ArrayList;
-
-
-import org.motion.ballsimapp.gwtsafe.Quartic;
-import org.motion.ballsimapp.shared.FieldVerifier;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.widgetideas.graphics.client.Color;
 import com.google.gwt.widgetideas.graphics.client.GWTCanvas;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Ballsimapp implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
 
-	
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final TableServiceAsync tableService = GWT
-			.create(TableService.class);
-	
-    private native void startJs()/*-{
-    
-    $wnd.initAll();
-    
-    }-*/;
+	Timer timer;
 	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
 		
-		final Button sendButton = new Button("Send");
+		final Button sendButton = new Button("Save");
 		final TextBox nameField = new TextBox();
 	       
-		double[] coeffs = { 24, -50, 35, -10, 1 };
 	        
-		nameField.setText("quartic:"+Quartic.smallestRoot(coeffs,100));
+		nameField.setText("empty");
 		final Label errorLabel = new Label();
-
-		final ListBox lb = new ListBox();
 		
-		final Button freetableButton = new Button("Free Tables");
-
-		startJs();
-		
- 
 		// We can add style names to widgets
 		sendButton.addStyleName("sendButton");
 
@@ -88,188 +39,27 @@ public class Ballsimapp implements EntryPoint {
 		RootPanel.get("sendButtonContainer").add(sendButton);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
 
-		RootPanel.get("freetableButtonContainer").add(freetableButton);
-		freetableButton.setEnabled(false);
-		RootPanel.get("freetableList").add(lb);
 		
 		// Focus the cursor on the name field when the app loads
 		nameField.setFocus(true);
 		nameField.selectAll();
 
 		
-	    // Make a new canvas 400x400 pixels
-	    GWTCanvas canvas = new GWTCanvas(400,400);
-	    
-	    canvas.setLineWidth(1);
-	    canvas.setStrokeStyle(Color.GREEN);
-	    
-	    canvas.beginPath();
-	      canvas.moveTo(1,1);
-	      canvas.lineTo(1,50);
-	      canvas.lineTo(50,50);
-	      canvas.lineTo(50, 1);
-	      canvas.closePath();
-	    canvas.stroke();
-	    
+	    // Make a new canvas 
+		GWTCanvas canvas = new GWTCanvas(300, 600);
+		
+	    final CanvasTable canvasTable = new CanvasTable(canvas);
+
 	    RootPanel.get().add(canvas);
 		
-		// Create the popup dialog box
-		final DialogBox dialogBox2 = new DialogBox();
-		dialogBox2.setText("Test");
-		dialogBox2.setAnimationEnabled(true);
-		final Button closeButton2 = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton2.getElement().setId("closeButton2");
-		VerticalPanel dialogVPanel2 = new VerticalPanel();
-		dialogVPanel2.addStyleName("dialogVPanel");
-		dialogVPanel2.add(new HTML("<b>Test box</b>"));
-		//dialogVPanel2.add(new HTML("<img ref=\"table.jpg\" />"));
-		
-		Image widget = new Image("table.jpg");
-		dialogVPanel2.add(widget);
-		
-		dialogVPanel2.add(closeButton2);
-
-		Image widget2 = new Image("ball.gif");
-		dialogVPanel2.add(widget2);
-
-		dialogBox2.setWidget(dialogVPanel2);
-		dialogBox2.setText("table");
-		//dialogBox2.show();
-		// Add a handler to close the DialogBox
-		closeButton2.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox2.hide();
-			}
-		});
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-//								dialogBox.setText("Remote Procedure Call");
-//								serverResponseLabel
-//										.removeStyleName("serverResponseLabelError");
-//								serverResponseLabel.setHTML(result);
-//								dialogBox.center();
-//								closeButton.setFocus(true);
-								freetableButton.setEnabled(true);
-
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
-		
-		class TableHandler implements ClickHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				freetableButton.setEnabled(false);
-				tableService.getTables(
-						new AsyncCallback<ArrayList<String>>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(ArrayList<String> tables) {
-
-								lb.clear();
-								for (String t : tables)
-									lb.addItem(t);
-								freetableButton.setEnabled(true);
-
-							}
-						});
-
-			}
-		}	
-
-		TableHandler handlerT = new TableHandler();
-		freetableButton.addClickHandler(handlerT);
+	    timer = new Timer() {
+	    	public void run() {
+	    	    canvasTable.renderLoop();
+	    	}
+	    };
+	    
+	    timer.scheduleRepeating(25);
+	    	    
 	}
 	
 	
