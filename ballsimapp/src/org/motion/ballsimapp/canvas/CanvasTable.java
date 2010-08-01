141e2229fbb5dbcb9fbe8d5d5fd6bb6ed66b9736
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-08-01 10:36:09 +0000

    image resource, tidy up.

--- ballsimapp/src/org/motion/ballsimapp/canvas/CanvasTable.java
+++ ballsimapp/src/org/motion/ballsimapp/canvas/CanvasTable.java
@@ -1,25 +1,47 @@
 package org.motion.ballsimapp.canvas;
 
+import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.widgetideas.graphics.client.GWTCanvas;
+import com.google.gwt.widgetideas.graphics.client.ImageLoader;
 
 public class CanvasTable {
 
 	private GWTCanvas canvas;
+	
+	ImageElement bg;
 	
 	int width = 20;
 	public CanvasTable(GWTCanvas canvas_)
 	{
 		canvas = canvas_;
 		
+	    String[] urls = new String[] {Resources.INSTANCE.logo().getURL()};
+
+	    ImageLoader.loadImages(urls, new ImageLoader.CallBack()
+	    {
+	          public void onImagesLoaded(ImageElement[] imageElements)
+	          {
+	              bg = imageElements[0];
+	          }
+	    });	
 	}
 	
 	public void renderLoop()
 	{
-		if (width>500)
+		if (width>600)
 			return;
 		canvas.clear();
 
-	    canvas.stroke();
+		if(bg!=null)
+		{
+			canvas.saveContext();
+			canvas.scale(1.4, 1.4);
+			canvas.drawImage(bg, 0, 0);			
+			canvas.scale(1, 1);
+			canvas.restoreContext();
+		}
+		
+		canvas.stroke();
 	    width++;
 	}
 }
