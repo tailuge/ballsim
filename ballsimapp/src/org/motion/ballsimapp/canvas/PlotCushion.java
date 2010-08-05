cba032fa2be8b7a34b9e86e351bd75e1335cb154
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-08-05 12:50:21 +0000

    plot path

---
+++ ballsimapp/src/org/motion/ballsimapp/canvas/PlotCushion.java
@@ -0,0 +1,30 @@
+package org.motion.ballsimapp.canvas;
+
+
+import org.motion.ballsimapp.logic.Cushion;
+
+import com.google.gwt.widgetideas.graphics.client.Color;
+import com.google.gwt.widgetideas.graphics.client.GWTCanvas;
+
+
+public class PlotCushion 
+{
+	
+	public static void plot(PlotScale scale, GWTCanvas canvas)
+ 	{
+		canvas.setStrokeStyle(Color.GREEN);
+
+		int xp = scale.scaledX(Cushion.xp)+scale.r;
+		int xn = scale.scaledX(Cushion.xn)-scale.r;
+		int yp = scale.scaledY(Cushion.yp)+scale.r;
+		int yn = scale.scaledY(Cushion.yn)-scale.r;
+		
+		canvas.moveTo(xn, yn);
+		canvas.lineTo(xp, yn);
+		canvas.lineTo(xp, yp);
+		canvas.lineTo(xn, yp);
+		canvas.lineTo(xn, yn);
+				
+ 	}
+
+}
