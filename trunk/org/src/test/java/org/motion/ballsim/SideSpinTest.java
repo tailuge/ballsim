5bbc0ee80d9397997b59ada620911c5b3cd0ea54
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2010-09-05 02:17:20 +0000

    start side spin code

---
+++ trunk/org/src/test/java/org/motion/ballsim/SideSpinTest.java
@@ -0,0 +1,42 @@
+package org.motion.ballsim;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.motion.ballsim.gwtsafe.Vector3D;
+
+
+public class SideSpinTest {
+
+	
+	@Test
+	public final void testXCollisionsNoSpin() 
+	{
+		// place near cushion
+		Event e = Utilities.getRolling(Vector3D.PLUS_I.scalarMultiply(-50));
+		e.pos = Vector3D.PLUS_I.scalarMultiply(Cushion.xn+Ball.R);
+		Event c1 = Cushion.hit(e, e.next().t);
+		assertNotNull("Collides",c1);
+		assertTrue("Collision before ball stops",c1.t < e.next().t);
+		assertEquals("Collision should be at cushion position",Cushion.xn,c1.pos.getX(),0.001);
+		assertTrue("Velocity reflected",c1.vel.getX() > 0);
+	}
+
+	@Ignore
+	@Test
+	public final void testXCollisionsPositiveSpin() 
+	{
+		// place near cushion
+		Event e = Utilities.getRolling(Vector3D.PLUS_I.scalarMultiply(-50));
+		e.pos = Vector3D.PLUS_I.scalarMultiply(Cushion.xn+Ball.R);
+		Event c1 = Cushion.hit(e, e.next().t);
+		assertNotNull("Collides",c1);
+		assertTrue("Collision before ball stops",c1.t < e.next().t);
+		assertEquals("Collision should be at cushion position",Cushion.xn,c1.pos.getX(),0.001);
+		assertTrue("Velocity reflected",c1.vel.getY() > 0);
+	}
+
+}
