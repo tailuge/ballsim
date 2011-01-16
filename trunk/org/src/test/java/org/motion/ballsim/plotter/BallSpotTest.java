4def8d3716341fe696236096a14c73b67cc26c1f
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-01-16 09:51:30 +0000

    Incomplete - task : get svn shadow set up

--- trunk/org/src/test/java/org/motion/ballsim/plotter/BallSpotTest.java
+++ trunk/org/src/test/java/org/motion/ballsim/plotter/BallSpotTest.java
@@ -7,13 +7,15 @@ import org.junit.Test;
 public class BallSpotTest {
 
 	@Test
-	public void testGetOffsetX() {
+	public void testStartAtTop() {
 		assertTrue("Initial pos at top",BallSpot.getOffsetX(0, 0) == 0);
+		assertTrue("Initial pos at top",BallSpot.getOffsetY(0, 0) == 0);
 	}
 
 	@Test
-	public void testGetOffsetY() {
-		assertTrue("Initial pos at top",BallSpot.getOffsetY(0, 0) == 0);
+	public void testAtEdgeStationary() {
+		assertEquals("spot at edge does not move",
+				BallSpot.getOffsetY(Math.PI/2, 0) , BallSpot.getOffsetY(Math.PI/2, 1),0.00001);		
 	}
 
 }
