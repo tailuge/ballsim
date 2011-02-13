925439baab67a66a7f28defc6cb6d015ced2a6ae
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-02-13 03:33:49 +0000

    out of knuckle test

--- org/src/test/java/org/motion/ballsim/PocketTest.java
+++ org/src/test/java/org/motion/ballsim/PocketTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.motion.ballsim.gwtsafe.Vector3D;
 import org.motion.ballsim.motion.Ball;
+import org.motion.ballsim.motion.Cushion;
 import org.motion.ballsim.motion.Event;
 import org.motion.ballsim.motion.EventType;
 import org.motion.ballsim.motion.Pocket;
 import org.motion.ballsim.motion.State;
+import org.motion.ballsim.util.UtilEvent;
 
 
 public class PocketTest {
@@ -54,25 +56,33 @@ public class PocketTest {
 	}
 	
 	@Test
-	public final void testPocketThenOtherCushion() 
+	public final void testBallIsInJaws() 
 	{
-		// start in pocket beyond cushion, but still on table, fire outwards.
-		// TODO
+		Event e = getBallInPocketJaws();
+		assertTrue("is beyond cushion in jaw",e.pos.getX() > Cushion.xp);
+		System.out.println(e);		
+	}
+
+	@Test
+	public final void testRollOutOfJaws() 
+	{
+		Event e = getBallInPocketJaws();
+		e.vel=Vector3D.MINUS_I.scalarMultiply(50);
+		e.state=State.deriveStateOf(e);
 		
+		Event c1 = e.next();
+		System.out.println(c1);				
+		assertTrue("on table after next state",c1.pos.getX() < Cushion.xp);		
+		
+		assertNull(Cushion.hit(e, c1.t, true));
+	}
+
+	private final Event getBallInPocketJaws()
+	{
 		Event e = Utilities.getRolling(Vector3D.PLUS_I.scalarMultiply(250));
 		e.pos = Pocket.p6k1.add(new Vector3D(-3*Ball.R,+0.2,0));
 		Event c1 = Pocket.nextKnuckleCollision(e, e.next().t);
-
-		assertNotNull("Collides",c1);
-		assertTrue("event is after time zero",c1.t > 0);
-		assertTrue("velocity is reflected",c1.vel.getX() < 0);
-		assertTrue("velocity is reflected partially downward",c1.vel.getY() > 0);
-		assertTrue("ball is sliding",c1.state == State.Sliding);
-		assertTrue("event type is knuckle collision",c1.type == EventType.KnuckleCushion);
 		
-		System.out.println(e);
-		System.out.println(c1);
-		
+		return UtilEvent.stationary(c1.pos);
 	}
-
 }
