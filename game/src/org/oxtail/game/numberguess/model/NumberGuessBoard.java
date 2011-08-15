2dbb8122137f926e08dcc8fda86fc892dd7e85fd
liamjknox@gmail.com <liamjknox@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-08-15 13:14:01 +0000

    add

--- game/src/org/oxtail/game/numberguess/model/NumberGuessBoard.java
+++ game/src/org/oxtail/game/numberguess/model/NumberGuessBoard.java
@@ -4,11 +4,12 @@ import java.util.Map;
 
 import org.oxtail.game.model.Player;
 import org.oxtail.game.model.PlayingSpace;
+import org.springframework.util.Assert;
 
 import com.google.common.collect.Maps;
 
 /**
- * Board for a simple number guess game
+ * Board for a simple number guess game i.e. numbers from 0 to 9 
  * 
  * @author liam knox
  */
@@ -16,10 +17,24 @@ public class NumberGuessBoard extends PlayingSpace {
 
 	private final Map<Integer, Player> guessBoard = Maps.newHashMap();
 
+	private final int startNumber;
+	private final int endNumber;
+
+	public NumberGuessBoard(int startNumber, int endNumber) {
+		Assert.isTrue(startNumber <= endNumber, "invalid number range, "
+				+ startNumber + " to " + endNumber);
+		this.startNumber = startNumber;
+		this.endNumber = endNumber;
+	}
+
 	public void guessNumber(Player player, int number) {
+		assertNumberValid(number);
+		guessBoard.put(number, player);
 	}
 
-	private void numberValid(int number) {
+	private void assertNumberValid(int number) {
+		Assert.isTrue(number >= startNumber);
+		Assert.isTrue(number <= endNumber);
+		Assert.isTrue(!guessBoard.containsKey(number), "number already guessed");
 	}
-
 }
