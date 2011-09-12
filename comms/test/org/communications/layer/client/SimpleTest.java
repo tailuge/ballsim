90c4bd1f091c9add746836ab491e72110ca726bf
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-12 19:35:16 +0000

    rpc test

--- comms/test/org/communications/layer/client/SimpleTest.java
+++ comms/test/org/communications/layer/client/SimpleTest.java
@@ -3,18 +3,51 @@ package org.communications.layer.client;
 
 import org.junit.Test;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class SimpleTest extends GWTTestCase{
 
-	public String getModuleName() {                                         // (2)
+	public String getModuleName() {                                         
 	    return "org.communications.layer.Comms";
 	  }
 	
 
 	@Test
 	public void testFirst()
-	{
-		assertTrue(true);
+	{		
+		assertTrue(new Integer(1) != null);
 	}
+
+	
+
+	@Test
+	public void testLogin()
+	{
+		GreetingServiceAsync greetingService = GWT
+		.create(GreetingService.class);
+
+		assertNotNull(greetingService);
+		
+		delayTestFinish(1000);
+		
+		greetingService.greetServer("username1",
+				new AsyncCallback<String>() {
+					public void onFailure(Throwable caught) {
+						System.out.println("caught:"+caught.getMessage());
+						assertTrue(false);
+						finishTest();
+					}
+
+					public void onSuccess(String result) {
+						System.out.println("ok");
+						assertTrue(true);
+						finishTest();
+					}
+				});
+		
+	}
+
+
 }
