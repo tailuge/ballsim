f969953ff34dcf0e7b4ce66612ab16a4a2bc2e42
liamjknox@gmail.com <liamjknox@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-17 08:03:20 +0000

    fixes

--- game/test/org/oxtail/game/numberguess/simulation/NumberGuessPlayer.java
+++ game/test/org/oxtail/game/numberguess/simulation/NumberGuessPlayer.java
@@ -18,11 +18,17 @@ import com.google.common.base.Supplier;
 
 public class NumberGuessPlayer implements Runnable, GameEventCallback {
 
+	private static final String[] messages = { "Arse Face", "God your Great",
+			"You smell of piss", "I Love You", "Chees and Beans", "Sheep",
+			"Gary Glitter", "Your Mother Sucks Cocks in Hell" };
+
 	private static final Random random = new Random();
 
 	private volatile boolean isRunning;
 
 	private Player player;
+
+	private volatile String gameId;
 
 	private GameStatemachine statemachine;
 
@@ -61,6 +67,7 @@ public class NumberGuessPlayer implements Runnable, GameEventCallback {
 		try {
 			log("thinking...");
 			think();
+			maybeChat();
 			PlayerState state = PlayerState.valueOf(player.getState());
 			switch (state) {
 			case LoggedOut:
@@ -73,6 +80,24 @@ public class NumberGuessPlayer implements Runnable, GameEventCallback {
 			log("fuck... " + e.getMessage());
 			e.printStackTrace(System.err);
 		}
+	}
+
+	private void maybeChat() {
+		GameEvent chat = newActionEvent("chat");
+		if (gameId != null) {
+			if (random.nextDouble() < 0.3) {
+				chat.addAttribute(new GameEventAttribute("game.id", gameId));
+				chat.addAttribute(new GameEventAttribute("chat.message",
+						pickMessage()));
+				chat.addAttribute(new GameEventAttribute("move", "-1"));
+				notify(chat);
+			}
+		}
+	}
+
+	private String pickMessage() {
+		int index = random.nextInt(messages.length);
+		return messages[index];
 	}
 
 	private void doWhenLoggedOut() {
@@ -105,7 +130,7 @@ public class NumberGuessPlayer implements Runnable, GameEventCallback {
 	}
 
 	private void log(String gameId, String message) {
-		System.err.println(format(gameId,message));
+		System.err.println(format(gameId, message));
 	}
 
 	private String format(String message) {
@@ -139,26 +164,42 @@ public class NumberGuessPlayer implements Runnable, GameEventCallback {
 		log("event received " + event.toString());
 		try {
 			GameEventHelper helper = new GameEventHelper(event);
+
 			if (helper.hasValue("game.id")) {
-				String gameId = helper.getString("game.id");
+
+				gameId = helper.getString("game.id");
+
+				if (chat(helper)) {
+					String message = helper.getString("chat.message");
+					String from = helper.getString("chat.from");
+					String to = helper.getString("chat.to");
+					log(gameId, "{" + from + "@" + to + "} '" + message + "'");
+				}
 				if (gameOver(helper)) {
 					String winner = helper.getString("player.winner");
 					if (isMe(winner))
-						log(gameId,"Hurray, I have Won!");
+						log(gameId, "Hurray, I have Won!");
 					else
-						log(gameId,"FUCK, I am Loser!");
+						log(gameId, "FUCK, I am Loser!");
 					isRunning = false;
 				} else if (inPlay(helper)) {
-					log(gameId,"to play, thinking...");
+					log(gameId, "to play, thinking...");
 					think();
 					int number = guessNumber();
-					log(gameId,"to play, guessed... " + number);
+					log(gameId, "to play, guessed... " + number);
 					notify(newMoveEvent(gameId, String.valueOf(number)));
 				}
+
+			} else {
+				gameId = null;
 			}
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 		}
+	}
+
+	private boolean chat(GameEventHelper helper) {
+		return helper.hasValue("chat.message");
 	}
 
 	private boolean gameOver(GameEventHelper helper) {
@@ -181,7 +222,10 @@ public class NumberGuessPlayer implements Runnable, GameEventCallback {
 
 	private boolean inPlay(GameEventHelper helper) {
 		String inplay = helper.getString("player.inplay");
-		return (getAlias().equals(inplay));
+		if (getAlias().equals(inplay)) {
+			return true;
+		}
+		return false;
 	}
 
 	private String getAlias() {
