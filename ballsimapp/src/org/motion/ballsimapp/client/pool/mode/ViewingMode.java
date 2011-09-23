23fad7b9b2049a26c644e0e3ce967962b20fa958
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-23 13:25:59 +0000

    refactor modes

--- ballsimapp/src/org/motion/ballsimapp/client/pool/mode/ViewingMode.java
+++ ballsimapp/src/org/motion/ballsimapp/client/pool/mode/ViewingMode.java
@@ -1,44 +1,41 @@
 package org.motion.ballsimapp.client.pool.mode;
 
+import static org.motion.ballsimapp.shared.GameEventUtil.*;
+
 import org.motion.ballsimapp.client.pool.BilliardsMarshaller;
-import org.motion.ballsimapp.client.pool.BilliardsPresenter;
+import org.motion.ballsimapp.client.pool.BilliardsModel;
+import org.motion.ballsimapp.client.pool.BilliardsView;
 import org.motion.ballsimapp.shared.GameEvent;
 
 import com.google.gwt.core.client.GWT;
 
 public class ViewingMode extends BilliardsMode {
 
-	public ViewingMode(BilliardsPresenter presenter) {
-		super(presenter);
+	public ViewingMode(BilliardsModel model, BilliardsView view) {
+		super(model, view);
 	}
 
 	@Override
 	public BilliardsMode handle(GameEvent event) {
-		
-		if (event.hasAttribute("aimUpdate"))
-		{
-			presenter.view.setAim(BilliardsMarshaller.aimFromEvent(event));
+
+		if (event.hasAttribute(AIM_UPDATE)) {
+			view.setAim(BilliardsMarshaller.aimFromEvent(event));
 			return this;
 		}
 
-		if (event.hasAttribute("aimComplete"))
-		{
-			presenter.model.updateWithHit(BilliardsMarshaller.aimFromEvent(event));
-			
-			// update view				
-			presenter.view.appendMessage("animate");
-			presenter.view.animate(presenter.model.table);
+		if (event.hasAttribute(AIM_COMPLETE)) {
+			model.updateWithHit(BilliardsMarshaller.aimFromEvent(event));
+			view.animate(model.table);
 			return this;
 		}
 
-		if (event.hasAttribute("animationComplete"))
-		{
-			presenter.model.resetForNextShot();
-			presenter.view.showTable(presenter.model.table);
+		if (event.hasAttribute(ANIMATION_COMPLETE)) {
+			model.resetForNextShot();
+			view.showTable(model.table);
 			return this;
 		}
-		
-		GWT.log("ViewingMode handled unexpected event:"+event);
+
+		GWT.log("ViewingMode handled unexpected event:" + event);
 
 		return this;
 	}
