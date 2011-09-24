c60a8df939ec572922922e1e127953ff00c7f57f
liamjknox@gmail.com <liamjknox@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-24 06:41:20 +0000

    fixes

--- game/src/org/oxtail/game/billiards/simplepool/state/PlayerState.java
+++ game/src/org/oxtail/game/billiards/simplepool/state/PlayerState.java
@@ -4,7 +4,7 @@ import org.oxtail.game.model.Player;
 
 public enum PlayerState {
 
-	InPlay, LoggedIn, LoggedOut, AwaintingGame;
+	InPlay, LoggedIn, LoggedOut, AwaitingGame;
 
 	public void set(Player... players) {
 		for (Player p : players)
