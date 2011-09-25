69a8be910989940bcd7db3c6642bbc0dd09cefb6
liamjknox@gmail.com <liamjknox@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-25 08:48:53 +0000

    players added

--- ballsimapp/src/org/motion/ballsimapp/proxy/GameServerProxy.java
+++ ballsimapp/src/org/motion/ballsimapp/proxy/GameServerProxy.java
@@ -3,28 +3,49 @@ package org.motion.ballsimapp.proxy;
 import org.motion.ballsimapp.shared.GameEvent;
 import org.motion.ballsimapp.shared.GameEventCallback;
 import org.motion.ballsimapp.shared.GameServer;
+import org.oxtail.game.billiards.simplepool.state.PlayerState;
 import org.oxtail.game.billiards.simplepool.state.SimplePoolStatemachine;
 import org.oxtail.game.home.GameHome;
 import org.oxtail.game.home.event.CallbackEnrichedGameHome;
 import org.oxtail.game.home.inmemory.InMemoryGameHome;
+import org.oxtail.game.model.Player;
 import org.oxtail.game.state.GameStatemachine;
 import org.oxtail.game.state.reflect.ReflectStateActionExecutor;
 import org.oxtail.game.state.reflect.ReflectStateFactory;
 
+/**
+ * Interface from GWT World to State machine world
+ */
 public class GameServerProxy implements GameServer {
 
-	private GameStatemachine statemachine;
+	private final GameStatemachine statemachine;
+	private final GameHome gameHome;
 
 	public GameServerProxy(GameEventCallback callback) {
-		GameHome home = new CallbackEnrichedGameHome(new InMemoryGameHome(),
+		gameHome = new CallbackEnrichedGameHome(new InMemoryGameHome(),
 				callback);
-		statemachine = new SimplePoolStatemachine(home,
+		statemachine = new SimplePoolStatemachine(gameHome,
 				new ReflectStateFactory(), new ReflectStateActionExecutor());
+		setUpPlayers("jim", "bob");
 	}
 
 	@Override
 	public void notify(GameEvent event) {
 		statemachine.execute(event);
+	}
+
+	/**
+	 * Temporary to hard code some players,
+	 */
+	private void setUpPlayers(String... playerAliases) {
+		for (String playerAlias : playerAliases)
+			setUpPlayer(playerAlias);
+	}
+
+	private void setUpPlayer(String playerAlias) {
+		Player player = new Player(playerAlias);
+		PlayerState.LoggedOut.set(player);
+		gameHome.storePlayer(player);
 	}
 
 }
