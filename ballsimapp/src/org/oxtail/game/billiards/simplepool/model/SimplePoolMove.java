a98f4d12e6db67318339e970066295cb0ad6796e
liamjknox@gmail.com <liamjknox@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-26 12:15:00 +0000

    fixes

--- ballsimapp/src/org/oxtail/game/billiards/simplepool/model/SimplePoolMove.java
+++ ballsimapp/src/org/oxtail/game/billiards/simplepool/model/SimplePoolMove.java
@@ -3,6 +3,8 @@ package org.oxtail.game.billiards.simplepool.model;
 import java.util.List;
 
 import org.oxtail.game.billiards.model.BilliardBall;
+import org.oxtail.game.billiards.model.BilliardBallCategory;
+import org.oxtail.game.billiards.model.BilliardBallTableState;
 import org.oxtail.game.model.Move;
 import org.oxtail.game.model.Player;
 
@@ -10,18 +12,27 @@ import com.google.common.collect.Lists;
 
 public class SimplePoolMove extends Move {
 
-	private List<BilliardBall> balls = Lists.newArrayList();
+	private List<BilliardBall> potted = Lists.newArrayList();
 	private BilliardBall cueBall;
-	
+
 	public SimplePoolMove(Player player) {
 		super(player);
+		cueBall = new BilliardBall(SimplePoolBallCategory.CUE_BALL);
+		cueBall.setTableState(BilliardBallTableState.OnTable);
 	}
 
-	public List<BilliardBall> getBalls() {
-		return balls;
+	public List<BilliardBall> getPotted() {
+		return potted;
 	}
-	
-	public BilliardBall cueBall() { return cueBall; }
 
-	
+	public BilliardBall cueBall() {
+		return cueBall;
+	}
+
+	public void setBallAsPotted(int ballId) {
+		BilliardBallCategory ballCategory = SimplePoolBallCategory.values()[ballId];
+		BilliardBall ball = new BilliardBall(ballCategory);
+		ball.setTableState(BilliardBallTableState.Potted);
+		potted.add(ball);
+	}
 }
