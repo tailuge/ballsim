6172d9c378dfd78309fa642f33fae57c8b3b5401
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-27 12:16:36 +0000

    rack

--- trunk/org/src/main/java/org/motion/ballsim/physics/Table.java
+++ trunk/org/src/main/java/org/motion/ballsim/physics/Table.java
@@ -13,250 +13,224 @@ import org.motion.ballsim.util.Assert;
 import org.motion.ballsim.util.Logger;
 import org.motion.ballsim.util.UtilEvent;
 
-
 /**
  * @author luke
- *
- * Table holds a list of balls. By calling the generate method all future events
- * for these balls are computed. This allows an initial position to be evaluated
- * to the point all balls are at rest.
+ * 
+ *         Table holds a list of balls. By calling the generate method all
+ *         future events for these balls are computed. This allows an initial
+ *         position to be evaluated to the point all balls are at rest.
  * 
  */
-public class Table implements Serializable
-{
+public class Table implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -811803763165641433L;
 
-	private final static Logger logger = new Logger("Table",false);
-	
+	private final static Logger logger = new Logger("Table", false);
+
 	public static double maxVel = 60.0;
 	public static double maxAngVel = 220.0;
 	public static double accelRoll = -0.8;
 	public static double accelSlide = -15.0;
 
-	private Map<Integer,Ball> ballMap = new HashMap<Integer,Ball>();
+	private Map<Integer, Ball> ballMap = new HashMap<Integer, Ball>();
 
 	public boolean hasPockets;
-	
-	public Table()
-	{
+
+	public Table() {
 		hasPockets = false;
 	}
-	
-	public Table(boolean hasPockets_)
-	{
+
+	public Table(boolean hasPockets_) {
 		hasPockets = hasPockets_;
 	}
-	
+
 	/**
-	 * Given all balls that are on the table
-	 * check each to see when the next rolling or
-	 * stationary transition will occur
+	 * Given all balls that are on the table check each to see when the next
+	 * rolling or stationary transition will occur
 	 * 
 	 * @return
 	 */
-	public Event nextNatural() 
-	{
+	public Event nextNatural() {
 		Event next = null;
-		for(Ball ball : balls())
-		{
+		for (Ball ball : balls()) {
 			Event e = ball.lastEvent();
 			if (e.state.isMotionlessEndState())
 				continue;
-			
+
 			Event eNext = e.next();
-			if ((next == null) || (eNext.t < next.t))
-			{
+			if ((next == null) || (eNext.t < next.t)) {
 				next = eNext;
-				Assert.isTrue( next.t > e.t);
+				Assert.isTrue(next.t > e.t);
 			}
 		}
 
 		return next;
 	}
 
-
-
-
-	
-	public String toString()
-	{	
+	public String toString() {
 		return balls().toString();
 	}
 
-	public Collection<Event> getAllEvents() 
-	{
+	public Collection<Event> getAllEvents() {
 		Collection<Event> all = new ArrayList<Event>();
 
-		for(Ball ball : balls())
-		{
+		for (Ball ball : balls()) {
 			all.addAll(ball.getAllEvents());
 		}
-		
+
 		return all;
 	}
 
-
 	/**
-	 *  given a table with balls (possibly in motion) this method
-	 *  expands the event list until all balls are at rest.
-	 *  
+	 * given a table with balls (possibly in motion) this method expands the
+	 * event list until all balls are at rest.
+	 * 
 	 */
-	public int generateSequence()
-	{
-		int count=0;
-		
+	public int generateSequence() {
+		int count = 0;
+
 		while (generateNext())
 			count++;
 		return count;
 	}
-	
-	public void setAim(Aim aim)
-	{
+
+	public void setAim(Aim aim) {
 		Event cueBall = Interpolator.interpolate(ball(1), 0);
 		Event hit = UtilEvent.hit(cueBall.pos, aim.dir, aim.speed,
 				aim.spin.getY());
-		ball(1).setFirstEvent(hit);		
+		ball(1).setFirstEvent(hit);
 	}
-	
+
 	/**
-	 * Compare times of all possible next events and
-	 * add only the first to occur in the time line.
+	 * Compare times of all possible next events and add only the first to occur
+	 * in the time line.
 	 * 
 	 * @return true if there was a new event added
 	 */
-	public boolean generateNext()
-	{
+	public boolean generateNext() {
 		// next natural event
 		logger.info("Initial conditions for iteration:");
-		for(Ball ball : balls())
-		{
-			logger.info("Ball {} : {}", ball.id,ball.lastEvent().format());
-		}		
-				
+		for (Ball ball : balls()) {
+			logger.info("Ball {} : {}", ball.id, ball.lastEvent().format());
+		}
+
 		Event next = nextNatural();
 		if (next == null)
 			return false;
-		
+
 		// use bounds of this to look for next cushion collision
-		
-		Event nextCushion = Cushion.nextCushionHit(this, next.t);		
+
+		Event nextCushion = Cushion.nextCushionHit(this, next.t);
 		if ((nextCushion != null) && (nextCushion.t < next.t))
 			next = nextCushion;
-		
-		// use bounds of this to look for pocket collisions 
-		
-		if (hasPockets)
-		{
+
+		// use bounds of this to look for pocket collisions
+
+		if (hasPockets) {
 			Event nextKnuckle = Pocket.nextKnuckleCollision(this, next.t);
 			if ((nextKnuckle != null) && (nextKnuckle.t < next.t))
 				next = nextKnuckle;
-			
+
 			Event nextPocket = Pocket.nextPot(this, next.t);
 			if ((nextPocket != null) && (nextPocket.t < next.t))
-				next = nextPocket;			
+				next = nextPocket;
 		}
-		
+
 		// use bounds of these to look for next ball/ball collision
 
 		EventPair nextCollision = Collision.nextBallCollision(this, next.t);
 
 		// add the soonest of these outcomes
-		
-		if ((nextCollision == null) || (next.t < nextCollision.first.t))
-		{
+
+		if ((nextCollision == null) || (next.t < nextCollision.first.t)) {
 			logger.info("Single event");
 			logger.info("Ball {} : {}", next.ballId, next.format());
-			logger.info("nextCollision {}",nextCollision);
+			logger.info("nextCollision {}", nextCollision);
 			Assert.isTrue(Cushion.onTable(next));
 			this.ball(next.ballId).add(next);
-		}
-		else
-		{
-			logger.info("Collision event: {}",nextCollision);
-			logger.info("Collision event time: {}",nextCollision.first.t);
-			logger.info("Discarded single event: {}",next);
-			logger.info("Discarded single event time: {}",next.t);
-			logger.info("Collision 1: {}",nextCollision.first.format());
-			logger.info("Collision 2: {}",nextCollision.second.format());
+		} else {
+			logger.info("Collision event: {}", nextCollision);
+			logger.info("Collision event time: {}", nextCollision.first.t);
+			logger.info("Discarded single event: {}", next);
+			logger.info("Discarded single event time: {}", next.t);
+			logger.info("Collision 1: {}", nextCollision.first.format());
+			logger.info("Collision 2: {}", nextCollision.second.format());
 			Assert.isTrue(Cushion.onTable(nextCollision.first));
 			Assert.isTrue(Cushion.onTable(nextCollision.second));
 			this.ball(nextCollision.first.ballId).add(nextCollision.first);
 			this.ball(nextCollision.second.ballId).add(nextCollision.second);
 		}
-		
-		logger.info("Table:{}",this);
-		
+
+		logger.info("Table:{}", this);
+
 		Assert.isTrue(Cushion.validPosition(this));
 		Assert.isTrue(Collision.validPosition(this));
-		
+
 		return true;
 	}
 
-
-	public void reset()
-	{
-		for(Ball ball : balls())
-		{
+	public void reset() {
+		for (Ball ball : balls()) {
 			ball.resetToFirst();
 		}
 	}
 
-	public void beginNewShot()
-	{
-		for(Ball ball : balls())
-		{
+	public void beginNewShot() {
+		for (Ball ball : balls()) {
 			Event e = Interpolator.interpolate(ball, getMaxTime());
-			
-			e.t=0;
-			e.vel=Vector3D.ZERO;
-			e.angularVel=Vector3D.ZERO;
+
+			e.t = 0;
+			e.vel = Vector3D.ZERO;
+			e.angularVel = Vector3D.ZERO;
 			e.state = State.Stationary;
-			
+
 			ball.setFirstEvent(e);
 		}
 	}
 
-	public double getMaxTime() 
-	{
+	public double getMaxTime() {
 		double latest = 0;
-		for(Ball a : balls())
-		{
+		for (Ball a : balls()) {
 			if (a.lastEvent().t > latest)
-				latest = a.lastEvent().t; 
-		}		
+				latest = a.lastEvent().t;
+		}
 		return latest;
 	}
-	
 
-	public Collection<Ball> balls()
-	{
+	public Collection<Ball> balls() {
 		return ballMap.values();
 	}
-	
-	public Ball ball(int id)
-	{
+
+	public Ball ball(int id) {
 		if (!ballMap.containsKey(id))
 			ballMap.put(id, new Ball(id));
-			
+
 		return ballMap.get(id);
 	}
-	
-	public void placeBall(Vector3D pos)
-	{
+
+	public void placeBall(Vector3D pos) {
 		ball(1).setFirstEvent(UtilEvent.stationary(pos));
 	}
-	
+
 	// temp move out.
-	public void rack()
-	{
+	public void rack(String type, String seed) {
+
 		ball(1).setFirstEvent(UtilEvent.stationary(Vector3D.ZERO));
-		ball(2).setFirstEvent(
-				UtilEvent.stationary(new Vector3D(-Ball.R * 0.46, +Ball.R * 18,
-						0)));
-		ball(3).setFirstEvent(
-				UtilEvent.stationary(new Vector3D(Ball.R * 8, -Ball.R * 3, 0)));		
+
+		if (type.equals("SimplePool")) {
+			ball(2).setFirstEvent(
+					UtilEvent.stationary(new Vector3D(-Ball.R * 0.46,
+							+Ball.R * 18, 0)));
+			ball(3).setFirstEvent(
+					UtilEvent.stationary(new Vector3D(Ball.R * 8,
+							-Ball.R * 0.5, 0)));
+		}
+		/*
+		 * double x = Ball.R * 0.46; double y = Ball.R * 14;
+		 * 
+		 * ball(4).setFirstEvent( UtilEvent.stationary(new Vector3D(x, y, 0)));
+		 */
 	}
 }
