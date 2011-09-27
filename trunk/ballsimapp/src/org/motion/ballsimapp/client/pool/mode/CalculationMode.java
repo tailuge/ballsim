780999038119669386efce295ada8655bb457ac3
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-27 07:31:48 +0000

    Incremental calculation

---
+++ trunk/ballsimapp/src/org/motion/ballsimapp/client/pool/mode/CalculationMode.java
@@ -0,0 +1,76 @@
+package org.motion.ballsimapp.client.pool.mode;
+
+import static org.motion.ballsimapp.shared.Events.AIM_COMPLETE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.motion.ballsim.game.Aim;
+import org.motion.ballsimapp.client.pool.BilliardsModel;
+import org.motion.ballsimapp.client.pool.BilliardsView;
+import org.motion.ballsimapp.shared.Events;
+import org.motion.ballsimapp.shared.GameEvent;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
+
+public class CalculationMode extends BilliardsMode implements RepeatingCommand {
+
+	final List<GameEvent> pending = new ArrayList<GameEvent>();
+	final Aim aim;
+	final boolean sendResult;
+
+	public CalculationMode(BilliardsModel model, BilliardsView view, Aim aim,
+			boolean sendResult) {
+		super(model, view);
+		this.sendResult = sendResult;
+		this.aim = aim;
+		model.table.setAim(aim);
+		pending.add(Events.event(AIM_COMPLETE, ""));
+		Scheduler.get().scheduleIncremental(this);
+	}
+
+
+	@Override
+	public boolean execute() {
+		boolean working = model.table.generateNext();
+		if (!working) {
+			if (sendResult) {
+				model.sendHit(aim);
+			}
+
+			Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+				@Override
+				public void execute() {
+					replayPendingEvents();
+				}
+			});
+		}
+		return working;
+	}
+
+	private void replayPendingEvents() {
+		for (GameEvent event : pending) {
+			model.sendToEventLoop(event);
+		}
+
+	}
+
+	@Override
+	public BilliardsMode handle(GameEvent event) {
+
+		if (event.hasAttribute(AIM_COMPLETE)) {
+			return new AnimationMode(model, view);
+		}
+
+		// Events received in this mode must be placed in
+		// pending list for replay when calculation is finished
+
+		pending.add(event);
+
+		GWT.log("CalculationMode added pending event:" + event);
+		return this;
+	}
+
+}
