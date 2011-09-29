8d079d7c2d48099caceb6395777256a3d77917ad
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-09-29 22:02:28 +0000

    single aim object

--- org/src/main/java/org/motion/ballsim/game/Aim.java
+++ org/src/main/java/org/motion/ballsim/game/Aim.java
@@ -4,11 +4,15 @@ import org.motion.ballsim.gwtsafe.Vector3D;
 
 public class Aim {
 	
+	final int ballId;
+	final public Vector3D pos;
 	final public Vector3D dir;
 	final public Vector3D spin;
 	final public double speed;
 	
-	public Aim(Vector3D dir, Vector3D spin, double speed) {
+	public Aim(int ballId, Vector3D pos,Vector3D dir, Vector3D spin, double speed) {
+		this.ballId = ballId;
+		this.pos = pos;
 		this.dir = dir;
 		this.spin = spin;
 		this.speed = speed;
