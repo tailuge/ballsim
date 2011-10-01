b854e35ee71aa38ba3dcc3bc5b6c159a83c5a52b
liamjknox@gmail.com <liamjknox@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-10-01 01:25:51 +0000

    add

--- trunk/org/src/test/java/org/motion/ballsim/PerformanceTest.java
+++ trunk/org/src/test/java/org/motion/ballsim/PerformanceTest.java
@@ -1,22 +1,51 @@
 package org.motion.ballsim;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.motion.ballsim.gwtsafe.Vector3D;
 import org.motion.ballsim.physics.Table;
+import org.motion.ballsim.util.Assert;
 import org.motion.ballsim.util.UtilEvent;
 
+/**
+ * Tests for shot simulation performance.
+ * <p>
+ * 100 iterations of break off<br>
+ * 1. No System.out: 167156<br> 
+ * 2. Vector3D final: 167468<br>
+ * 3. Other finalizations and log conditionals: 156703<br>
+ * 4. Full finalization and assert replace, assertions enabled: 44437<br>
+ * 5. Full finalization and assert replace, assertions disabled: 41907<br>
+ *
+ */
 public class PerformanceTest {
 
+	private int numberOfIterations = 100;
+	
+	@Before
+	public void before() {
+		
+	}
+	
+	private long now() { return System.currentTimeMillis(); }
+	
 	@Test
 	public final void hitSequenceOk() 
 	{
+		int numberOfEvents = doTest();
+		Assert.isTrue(numberOfEvents>150);
+		long start = now();
+		for (int i=0;i<numberOfIterations;++i)
+			doTest();
+		System.out.println(now() - start);
+	}
+	
+	private int doTest() {
 		Table t = new Table();
 		t.rack("9Ball", "");
 		t.ball(1).setFirstEvent(UtilEvent.hit(Vector3D.ZERO, Vector3D.PLUS_J, 2.6,0.1));
 		t.generateSequence();
-		System.out.println(t.getAllEvents().size() + "events");
-		assertTrue(t.getAllEvents().size() > 150);
+		return t.getAllEvents().size();
 	}
+
 }
