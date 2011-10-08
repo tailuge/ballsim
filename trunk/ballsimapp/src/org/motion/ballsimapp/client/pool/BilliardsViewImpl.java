73486b4b3ed4706d0d835f1373b6d6bf3275ae2e
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-10-08 21:41:56 +0000

    add shot timeout

--- trunk/ballsimapp/src/org/motion/ballsimapp/client/pool/BilliardsViewImpl.java
+++ trunk/ballsimapp/src/org/motion/ballsimapp/client/pool/BilliardsViewImpl.java
@@ -9,39 +9,54 @@ import org.motion.ballsimapp.client.comms.GWTGameEventHandler;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class BilliardsViewImpl extends BilliardsViewLayout implements
 		BilliardsView {
 
 	private final ChatView chatView;
-	
+	private final Timer timer = new Timer() {
+		public void run() {
+			appendMessage("out of time !");
+			hit();
+		}
+	};
+
 	public BilliardsViewImpl(int width, String layoutId, String defaultId) {
 		super(width, layoutId);
 
-		chatView = new ChatView(width,layoutId);
+		chatView = new ChatView(width, layoutId);
 		
 		playerId.setText(defaultId);
+
 		actionButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				actionButton.setEnabled(false);
-				eventHandler.handleEvent(BilliardsEventFactory
-							.inputComplete(new Aim(1,aim.getCueBallPosition(),aim.getAimDirection(),
-									spin.getSpin(), power.getPower())));
+				hit();
 			}
 		});
-		
+
 		loginButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				loginButton.setEnabled(false);
-				playerId.setEnabled(false);
-				eventHandler.handleEvent(BilliardsEventFactory.beginLogin());
+				login();
 			}
 		});
-				
-		
+
 	}
 
+	private void login() {
+		loginButton.setEnabled(false);
+		playerId.setEnabled(false);
+		eventHandler.handleEvent(BilliardsEventFactory.beginLogin());
+	}
+
+	private void hit() {
+		timer.cancel();
+		actionButton.setEnabled(false);
+		eventHandler.handleEvent(BilliardsEventFactory.inputComplete(new Aim(1,
+				aim.getCueBallPosition(), aim.getAimDirection(),
+				spin.getSpin(), power.getPower())));
+	}
 
 	@Override
 	public void appendMessage(String message) {
@@ -67,6 +82,7 @@ public class BilliardsViewImpl extends BilliardsViewLayout implements
 		actionButton.setText("Hit");
 		actionButton.setEnabled(true);
 		aim.aim();
+		timer.schedule(timeout * 1000);
 	}
 
 	@Override
@@ -75,6 +91,7 @@ public class BilliardsViewImpl extends BilliardsViewLayout implements
 		actionButton.setText("Place");
 		actionButton.setEnabled(true);
 		aim.place();
+		timer.schedule(timeout * 1000);
 	}
 
 	@Override
@@ -88,15 +105,14 @@ public class BilliardsViewImpl extends BilliardsViewLayout implements
 	}
 
 	@Override
-	public void setAim(Aim aim) {		
+	public void setAim(Aim aim) {
 		spin.setSpin(aim.spin);
 		power.setPower(aim.speed);
 		this.aim.setAim(aim);
 	}
 
 	@Override
-	public void showAim() 
-	{
+	public void showAim() {
 		aim.showAim();
 	}
 
@@ -120,23 +136,20 @@ public class BilliardsViewImpl extends BilliardsViewLayout implements
 		actionButton.setEnabled(false);
 	}
 
-
 	@Override
 	public void setVisibility(boolean visibility) {
 		actionButton.setVisible(visibility);
-		RootPanel.get(layoutId+".table").setVisible(visibility);
-		RootPanel.get(layoutId+".tableactive").setVisible(visibility);
-		RootPanel.get(layoutId+".inputspin").setVisible(visibility);
-		RootPanel.get(layoutId+".inputpower").setVisible(visibility);
-		RootPanel.get(layoutId+".tablebg").setVisible(visibility);
+		RootPanel.get(layoutId + ".table").setVisible(visibility);
+		RootPanel.get(layoutId + ".tableactive").setVisible(visibility);
+		RootPanel.get(layoutId + ".inputspin").setVisible(visibility);
+		RootPanel.get(layoutId + ".inputpower").setVisible(visibility);
+		RootPanel.get(layoutId + ".tablebg").setVisible(visibility);
 	}
-
 
 	@Override
 	public void setChatEnable(boolean enable) {
 		chatView.setChatEnable(enable);
 	}
-
 
 	@Override
 	public void clearMessage() {
