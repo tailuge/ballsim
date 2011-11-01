56b88cc280c2f4c9e3d95297558a5277791788ee
tailuge@gmail.com <tailuge@gmail.com@da898edf-300e-59e5-df47-6bac7c2f4a18>
2011-11-01 22:00:49 +0000

    switch in 3d view

--- trunk/ballsimapp/src/org/motion/ballsimapp/client/view3d/BilliardsView3DImpl.java
+++ trunk/ballsimapp/src/org/motion/ballsimapp/client/view3d/BilliardsView3DImpl.java
@@ -8,25 +8,35 @@ import java.util.List;
 import org.motion.ballsim.physics.Table;
 import org.motion.ballsim.physics.ball.Ball;
 import org.motion.ballsim.physics.ball.Event;
+import org.motion.ballsim.physics.ball.State;
 import org.motion.ballsim.physics.game.Aim;
 import org.motion.ballsim.physics.gwtsafe.Vector3D;
 import org.motion.ballsim.physics.util.Interpolate;
+import org.motion.ballsimapp.canvas.Animation;
 import org.motion.ballsimapp.client.comms.GWTGameEventHandler;
 import org.motion.ballsimapp.client.pool.TableView;
 
 public class BilliardsView3DImpl extends Inputs implements TableView {
 
 	private Table table;
+	private boolean aiming = false;
 	
-	public BilliardsView3DImpl(String layoutId) {
-		super(layoutId);
+	public BilliardsView3DImpl(int width, String layoutId) {
+		super(width, layoutId);
 	}
 
+	@Override
 	public void showTable(Table table) {
 		this.table = table;
 		plotAtTime(table, 0);
 	}
 
+	public void showTable() {
+		if (table != null)
+			plotAtTime(table, 0);
+	}
+
+	@Override
 	public void plotAtTime(Table table, double t) {
 		this.table = table;
 		List<Event> events = new ArrayList<Event>();
@@ -37,8 +47,13 @@ public class BilliardsView3DImpl extends Inputs implements TableView {
 		prepareDraw();
 
 		inputPos = events.get(0).pos;
-		//Aim aim = new Aim(0, inputPos, inputDir, inputSpin,	0);
-
+		Aim aim = new Aim(0, inputPos, inputDir, inputSpin,	0);
+		
+		if (aiming)
+			camera.setAimView(aim.pos, aim.dir);
+		else
+			camera.setSpectatorView(aim.pos, aim.dir);
+			
 		//setAim(aim);
 		
 		MODELVIEW.push();
@@ -49,15 +64,20 @@ public class BilliardsView3DImpl extends Inputs implements TableView {
 		placeTable();
 
 		updateThrust(t);
-		placeCue(inputPos, inputAngle, inputSpin, cueSwing);
+		if (aiming)
+			placeCue(inputPos, inputAngle, inputSpin, cueSwing);
 
 		MODELVIEW.pop();
 
 	}
 
 	private void plotBall(Event event) {
+		if (event.state == State.InPocket)
+			return;
+
 		Vector3D a = Vector3D.crossProduct(event.angularPos,
 				event.angularPosPerp);
+		
 		placeBall(event.ballId - 1, event.pos.getX(), event.pos.getY(),
 				event.angularPos.getX(), event.angularPos.getY(),
 				event.angularPos.getZ(), event.angularPosPerp.getX(),
@@ -68,56 +88,47 @@ public class BilliardsView3DImpl extends Inputs implements TableView {
 	@Override
 	public void setAim(Aim aim) {
 		camera.setAimView(aim.pos, aim.dir);
+	}
+
+	@Override
+	public void setEventHandler(GWTGameEventHandler eventHandler) {
+		this.eventHandler = eventHandler;
+	}
+
+	@Override
+	public void aim(int timeout) {
+		aiming = true;		
+	}
+
+	@Override
+	public void place(int timeout) {
+	}
+
+	@Override
+	public void watch() {
+		aiming = false;		
+	}
+
+	@Override
+	public void animate(Table table) {
+		this.table = table;
+		@SuppressWarnings("unused")
+		Animation showAnimation = new Animation(table, this, eventHandler);
+	}
+
+
+	@Override
+	public void showAim() {
 		if (table != null)
 			showTable(table);
 	}
 
 	@Override
-	public void setEventHandler(GWTGameEventHandler eventHandler) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void aim(int timeout) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void place(int timeout) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void watch() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void animate(Table shotSequence) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void showAim() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void showPlacer() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void setVisibility(boolean visible) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
--- trunk/ballsimapp/src/org/motion/ballsimapp/client/view3d/Inputs.java
+++ trunk/ballsimapp/src/org/motion/ballsimapp/client/view3d/Inputs.java
@@ -1,6 +1,9 @@
 package org.motion.ballsimapp.client.view3d;
 
+import org.motion.ballsim.physics.game.Aim;
 import org.motion.ballsim.physics.gwtsafe.Vector3D;
+import org.motion.ballsimapp.client.comms.GWTGameEventHandler;
+import org.motion.ballsimapp.client.pool.BilliardsEventFactory;
 
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.MouseDownEvent;
@@ -30,13 +33,15 @@ public class Inputs extends Render implements MouseDownHandler, MouseUpHandler,
 	private int initialX = 0;
 	private int initialY = 0;
 	private double initialAngle = 0;
-	
+
+	GWTGameEventHandler eventHandler;
+
 	/** Mouse handler registration */
 	private HandlerRegistration mouseDownRegistration, mouseUpRegistration,
 			mouseMoveRegistration, mouseWheelHandler;
 
-	public Inputs(String layoutId) {
-		super(layoutId);
+	public Inputs(int width, String layoutId) {
+		super(width, layoutId);
 		mouseDownRegistration = surface.addMouseDownHandler(this);
 		mouseUpRegistration = surface.addMouseUpHandler(this);
 		mouseMoveRegistration = surface.addMouseMoveHandler(this);
@@ -50,15 +55,11 @@ public class Inputs extends Render implements MouseDownHandler, MouseUpHandler,
 		mouseWheelHandler.removeHandler();
 	}
 
-	final double width = 500;
-	final double height = 500;
-	
-	protected void updateThrust(double t)
-	{
+	protected void updateThrust(double t) {
 		if (swingBegin == 0)
 			swingBegin = t;
-		
-		cueSwing = Math.sin((t-swingBegin) * 5.0 * inputSpeed);
+
+		cueSwing = Math.sin((t - swingBegin) * 5.0 * inputSpeed);
 		cueSwing *= cueSwing;
 	}
 
@@ -74,42 +75,63 @@ public class Inputs extends Render implements MouseDownHandler, MouseUpHandler,
 	public void onMouseMove(MouseMoveEvent event) {
 		if (!active)
 			return;
-		
-		if (event.getNativeButton() == NativeEvent.BUTTON_RIGHT)
-		{
-			DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor", "all-scroll");
-			double horizontal = 3.5 * (double) (event.getX()-initialX) / width;
-			double vertical = 3.5 * (double) (event.getY()-initialY) / height;
-			if (vertical > 1) vertical = 1;
-			if (vertical < -1) vertical = -1;
-			if (horizontal > 1) horizontal = 1;
-			if (horizontal < -1) horizontal = -1;
-			inputSpin = new Vector3D(horizontal,vertical,0);
-			swingBegin = 0;
+
+		if (event.getNativeButton() == NativeEvent.BUTTON_MIDDLE)
 			return;
+		
+		if (event.getNativeButton() == NativeEvent.BUTTON_RIGHT) {
+			DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor",
+					"all-scroll");
+			double horizontal = 3.5 * (double) (event.getX() - initialX)
+					/ width;
+			double vertical = 3.5 * (double) (event.getY() - initialY) / width;
+			if (vertical > 1)
+				vertical = 1;
+			if (vertical < -1)
+				vertical = -1;
+			if (horizontal > 1)
+				horizontal = 1;
+			if (horizontal < -1)
+				horizontal = -1;
+			inputSpin = new Vector3D(horizontal, vertical, 0);
+			swingBegin = 0;
+		} else {
+			DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor",
+					"crosshair");
+
+			int offset = event.getX() - initialX;
+			inputAngle = initialAngle + 0.1 * Math.signum(offset)
+					* (Math.pow(Math.abs(offset), 1.7) / width) * Math.PI;
+			inputDir = new Vector3D(Math.sin(inputAngle), Math.cos(inputAngle),
+					0);
+			swingBegin = 0;
 		}
 		
-	    DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor", "crosshair");
-	
-	    int offset = event.getX()-initialX;
-		inputAngle = initialAngle + 0.1 * Math.signum(offset)*(Math.pow(Math.abs(offset),1.7) / width) * Math.PI;
-		inputDir = new Vector3D(Math.sin(inputAngle),Math.cos(inputAngle), 0);
-		swingBegin = 0;
+		eventHandler.handleEvent(BilliardsEventFactory.cursorInput(getAim()));
+
 	}
 
 	@Override
 	public void onMouseUp(MouseUpEvent event) {
 		active = false;
 		DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor", "default");
+		if (event.getNativeButton() == NativeEvent.BUTTON_MIDDLE) {
+			eventHandler.handleEvent(BilliardsEventFactory.inputComplete(getAim()));
+		}
 	}
-
 
 	@Override
 	public void onMouseWheel(MouseWheelEvent event) {
 		swingBegin = 0;
 		inputSpeed += event.isNorth() ? 0.1 : -0.1;
-		if (inputSpeed < 0.1) inputSpeed = 0.1;
-		if (inputSpeed > 1.0) inputSpeed = 1.0;
+		if (inputSpeed < 0.1)
+			inputSpeed = 0.1;
+		if (inputSpeed > 1.0)
+			inputSpeed = 1.0;
 	}
 
+	private Aim getAim()
+	{
+		return new Aim(0, inputPos, inputDir, inputSpin, inputSpeed);
+	}
 }
